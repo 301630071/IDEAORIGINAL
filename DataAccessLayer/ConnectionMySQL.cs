8f072afa1f763f95577724cd4ae7f78ba36ef41e
Eduardo <Eduardo@EDUARDO-PC>
2017-03-21 15:43:46 -0600

    Se crea la conexión a la base de Datos "ConnectionsMySQL" y la cadena de conexión para que tenga comunicación a la BD

---
+++ DataAccessLayer/ConnectionMySQL.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ConnectionMySQL
+    {
+        //Declaramos la conexión a la Base de Datos en MySQL
+        public static MySqlConnection MySqlConn = new MySqlConnection();
+        //Declaramos la cadena de Conexión que utiliza la Base de datos en MySQL.
+        public static string ConnectionString = "server=localhost;user id=root; pwd=melapelas5225.;persistsecurityinfo=True;database=idea";
+
+        //Metodo que sirve para iniciar la conexión con MySQL
+
+        public static string iniciarConexion()
+        {
+            try
+            {
+                MySqlConn.ConnectionString = ConnectionString;
+                MySqlConn.Open();
+                return "";
+            }
+            catch (Exception ex)
+            {
+                //En caso de haber un error al intentar conectarse a la BD
+                //nos enviara un mensaje de error
+                return ex.Message;
+            }
+        }
+
+    }
+}
